f9352878f948b640c1e51e8abb197215fede8bcb
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2020-05-04 23:00:17 -0400

    Add Automatic Settings

--- set-settings.sh
+++ set-settings.sh
@@ -1,12 +1,12 @@
-rm -f -r ../alacritty
-rm -f -r ../qtile
-rm -f -r ../rofi
-rm -f -r ../Thunar
+rm -r ../alacritty
+rm -r ../qtile
+rm -r ../rofi
+rm -r ../Thunar
 rm ../.bashrc
-mv -f alacritty/* ../
-mv -f qtile/* ../
-mv -f rofi/* ../
-mv -f Thunar/* ../
+mv -f alacritty ../
+mv -f qtile ../
+mv -f rofi ../
+mv -f Thunar ../
 mv -f .bashrc ../
 rm -f -r ../ConfigsArchLinux
-cd ..
+cd
