99c629771c37b3e3bbe454f79a5a1157d11e8898
SergioRibera <sergioalejandroriberacosta@gmail.com>
2020-06-12 21:31:19 -0400

    Restore all

--- autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# systray battery icon
-#cbatticon -u 5 &
-feh -z --bg-fill ~/Images/bg
--- qtile/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# systray battery icon
-cbatticon -u 5 &
-feh --bg-randomize --bg-fill ~/Images
