92077e928024abd58b295ec013268b8242036ced
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2020-06-29 16:47:32 -0400

    Update autosettings.sh

--- autosettings.sh
+++ autosettings.sh
@@ -1,33 +1,35 @@
-if [ -d "~/.config/Thunar" ]; then
-  rm -fr ~/.config/Thunar
+if [ -d "$HOME/.config/Thunar" ]; then
+  rm -fr $HOME/.config/Thunar
 fi
-if [ -d "~/.config/alacritty" ]; then
-  rm -fr ~/.config/alacritty
+if [ -d "$HOME/.config/alacritty" ]; then
+  rm -fr $HOME/.config/alacritty
 fi
-if [ -d "~/.config/qtile" ]; then
-  rm -fr ~/.config/qtile
+if [ -d "$HOME/.config/qtile" ]; then
+  rm -fr $HOME/.config/qtile
 fi
-if [ -d "~/.config/rofi" ]; then
-  rm -fr ~/.config/rofi
+if [ -d "$HOME/.config/rofi" ]; then
+  rm -fr $HOME/.config/rofi
 fi
 cd ConfigsArchLinux
-mv Configs/Thunar ~/.config
-mv Configs/alacritty ~/.config
-mv Configs/qtile ~/.config
-mv Configs/rofi ~/.config
-mv Images ~/
-cp Configs/accels.scm ~/.config
-cp Configs/alacritty.yml ~/.config
-cp Configs/autostart.sh ~/.config
-cp Configs/config ~/.config
-cp Configs/config.py ~/.config
-cp Configs/mimeapps.list ~/.config
-cp Configs/theme.py ~/.config
-cp Configs/uca.xml ~/.config
-cp .bashrc ~/
-cp cbg.sh ~/
+if [ -d "Configs/Thunar" ]; then
+  mv Configs/Thunar $HOME/.config
+fi
+mv Configs/alacritty $HOME/.config
+mv Configs/qtile $HOME/.config
+mv Configs/rofi $HOME/.config
+mv Images $HOME/
+cp Configs/accels.scm $HOME/.config
+cp Configs/alacritty.yml $HOME/.config
+cp Configs/autostart.sh $HOME/.config
+cp Configs/config $HOME/.config
+cp Configs/config.py $HOME/.config
+cp Configs/mimeapps.list $HOME/.config
+cp Configs/theme.py $HOME/.config
+cp Configs/uca.xml $HOME/.config
+cp .bashrc $HOME/
+cp cbg.sh $HOME/
 sudo pacman -Syy figlet
-cd ~
+cd $HOME
 figlet -c Fin!!!
 figlet -c Sigueme
 echo ""
